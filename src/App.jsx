52cf1e0326abad3bd677f76d095801bff085baac
Nicolas Descarpentries <nicolas.descarp@outlook.fr>
2026-01-09 11:53:54 +0100

    feat: wrap app into wishlist context

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import { Routes, Route } from "react-router"
 import MovieList from './components/movieList/MovieList.jsx'
 import MovieDetail from './components/movieDetail/MovieDetail.jsx'
 
+import { WishlistProvider } from './contexts/WishlistContext';
+
 function App() {
   return (
+    <WishlistProvider>
       <Routes>
-        <Route path="/" element = {<MovieList/>}/>
-        <Route path="/movie/:id" element = {<MovieDetail/>}/>
+        <Route path="/" element={<MovieList />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
       </Routes>
+    </WishlistProvider>
     // <MovieList />
-    );
-  
+  );
+
 }
 
 export default App
